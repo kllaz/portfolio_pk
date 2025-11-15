4c35e05c71ac6f5f633a81787054803bb0a87389
kllaz <ghaklompsd@gmail.com>
2025-11-15 17:13:57 +0300

    breyta: vue fully designed

--- site/frontend/breyta/js/script.js
+++ site/frontend/breyta/js/script.js
@@ -1,3 +1,4 @@
+//vue activate
 const app = Vue.createApp({
     components:{
         ReportSlider
@@ -5,3 +6,25 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+//open analysis
+//const clickToOpen = document.getElementById('openAnalysis');
+function openAnalysis () {
+    const clickToOpen = document.getElementById('openAnalysis');
+    const fullAnalysis = document.getElementById('fullAnalysis');
+    const changeContent = document.getElementById('changeContent');
+    const svg = document.getElementById('svg');
+    if(clickToOpen.classList.contains('open')){
+        clickToOpen.classList.toggle('open');
+        clickToOpen.classList.toggle('hide');
+        changeContent.textContent = "Hide full analysis";
+        fullAnalysis.style.display = "block";
+        svg.style.transform = 'rotate(180deg)';
+    } else if(clickToOpen.classList.contains('hide')){
+        clickToOpen.classList.toggle('open');
+        clickToOpen.classList.toggle('hide');
+        changeContent.textContent = "See full analysis";
+        fullAnalysis.style.display = "none";
+        svg.style.transform = 'rotate(0deg)';
+    }
+}
